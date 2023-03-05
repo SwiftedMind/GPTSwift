f02df2be47c282ecf02d498f15db195e5ecc1b7a
Dennis Müller <d3mueller@me.com>
2023-03-05 09:27:10 +0100

    Adde documentation

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GPTSwift",
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "GPTSwift",
